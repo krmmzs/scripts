6bec965374f5ff79a07eef2beab8334add9b8fd0
krmmzs <belliyan250@gmail.com>
2022-10-03 02:57:28 +0800

    auto update

---
+++ auto_simple_git.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+LOG=`pwd`/log.txt
+OLD_LOG=`pwd`/log_old.txt
+mv ${LOG} ${OLD_LOG}
+
+for d in */ ; do
+    echo "$d"
+    cd "$d"
+    if ! [[ $(git status) =~ "working tree clean" ]]; then
+        git add . >> ${LOG} 2>&1
+        if ! [[ $(git commit -m "auto update") ]]; then
+            echo "$d commit failed" >> ${LOG}
+            echo "git commit failed, details see log.txt"
+        fi
+
+        if ! [[ $(git push) ]]; then
+            echo "$d push failed" >> ${LOG}
+            echo "git push failed, details see log.txt"
+        fi
+    fi
+
+
+    # git commit -m "auto update" >> ${LOG} 2>&1
+    # git push >> ${LOG} 2>&1
+
+    # if [[ 1 == ${rebase} ]]; then
+    #     git pull --rebase
+    # fi
+    # if [[ 1 == "${push}" ]]; then
+    #     git push
+    # fi
+    echo "$d" done
+    cd ..
+done
+${EDITOR} ${LOG}
